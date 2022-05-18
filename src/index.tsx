da92fd047b907ecd3951842900a3d9b1fd54d7d8
thiagofranchin <thiago.franchin@gmail.com>
2022-05-18 10:45:02 -0300

    Create API and Axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 4000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date()
+        }
+      ]
+    });
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
