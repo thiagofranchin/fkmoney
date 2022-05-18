61964c9f4b528f1d3839e1f7d63aefb302cc61bc
thiagofranchin <thiago.franchin@gmail.com>
2022-05-18 13:30:24 -0300

    React Modal structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleNewTransactionModalClose() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleNewTransactionModalOpen} />
       <Dashboard />
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleNewTransactionModalClose} // close modal when user clicks outside of it or ESC key is pressed
+      >
+        <h2>Nova transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
